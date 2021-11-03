6810eb7ce22223a9f042cf83b9f1402dd0d98c3b
RolandGulbinovic <roland.gulbinovicvu@gmail.com>
2021-11-03 08:21:09 -0700

    Update

---
+++ GUI/GUI.py
@@ -0,0 +1,186 @@
+
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import pandas as pd
+import webbrowser
+
+from scraping import get_markes, get_modeliai, get_category, get_detales
+
+
+# Graphical interfeisas
+def GUI(mark_list, mark_title, ft):
+    def get_tokens(words): # gauti atskirus zodzius
+        tokens = words.split()
+        return tokens
+
+    def relevant_words(searching): # naudojant fasttext paketo lietuvisku zodziu vektoriu randami 10 panasiausi zodziai
+        relevant_word_list = []
+        for i in searching:
+            relevant_word_list += ft.get_nearest_neighbors(i, k = 10)
+
+        for i in range(len(relevant_word_list)):
+            relevant_word_list[i] = relevant_words[i][1].lower()
+            
+        return relevant_word_list
+    
+    def mark_changed(event): # metodas, kad gauti markes modelius Combobox
+        global model_title
+        model_title, x = get_modeliai(Combo_mark.get(), mark_title)
+        Combo_model.config(value = x)
+
+    def model_changed(event): # metodas, kad gauti kategorijas listbox
+        global parts_title
+        parts_title, x = get_category(Combo_model.get(), model_title)
+    
+        for i in range(len(x)):
+            listbox.delete(i)
+        for i in range(len(x)):
+            listbox.insert(i, x[i])
+
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.pack_propagate(False)
+    root.resizable(0, 0)
+
+    frame1 = tk.LabelFrame(root, text = "Detales")
+    frame1.place(height=250, width=500)
+
+    file_frame = tk.LabelFrame(root, text = "Paieška")
+    file_frame.place(height = 70, width = 200, rely=0.65,relx = 0.60)
+
+    # TreeView, kad duomenys parodyti
+    tv1 = ttk.Treeview(frame1)
+    tv1.place(relheight=1,relwidth=1)
+
+    search = tk.Entry(file_frame, width = 15)
+    search.place(rely=0.40, relx = 0.25)
+
+    treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview)
+    treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview)
+    tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
+    treescrollx.pack(side="bottom", fill = "x")
+    treescrolly.pack(side="right", fill= "y")
+
+    button1 = tk.Button(root, text = "Visi prekės",height = 3, width = 20, command=lambda: Load_data())
+    button1.place(rely=0.80, relx=0.60)
+
+    button2 = tk.Button(file_frame, text = "Ieškoti", command=lambda: Search())
+    button2.place(rely=0.35, relx=0.65)
+    model_list = []
+    marke = tk.StringVar()
+    model = tk.StringVar()
+
+    
+    button3 = tk.Button(root, text = "Atidaryti", command=lambda: selectItem())
+    button3.place(rely=0.50, relx=0.75)
+    
+    
+    Combo_mark = ttk.Combobox(root, values = mark_list)
+    Combo_mark.set("Markė")
+    Combo_mark.place(relx = 0.05, rely = 0.55)
+    ## MARKE
+    Combo_mark.bind('<<ComboboxSelected>>',  mark_changed)
+
+
+    Combo_model = ttk.Combobox(root, values = [])
+    Combo_model.set("Modelis")
+    Combo_model.place(relx = 0.05, rely = 0.60)
+    Combo_model.bind('<<ComboboxSelected>>', model_changed)
+    ## Model
+
+    listbox = tk.Listbox(root, width = 47, selectmode = "SINGLE")
+    listbox.place(relx = 0, rely = 0.65)
+    
+    # metodas, kuris atidaro pasirinktos prekes puslapi
+    def selectItem():
+        curItem = tv1.focus()
+        webbrowser.open(tv1.item(curItem)['values'][4])
+    
+    # parodo duomenys TreeView
+    def Load_data():
+        clear_data()
+        global detales
+        detales = get_detales(listbox.get("anchor"), Combo_mark.get(), parts_title)\
+        
+        cols = detales.columns.tolist()
+        cols = cols[:3] + [cols[4]] +[cols[3]]
+        
+        detales = detales[cols]
+        
+        tv1["column"] = list(detales.columns)
+        tv1["show"] = "headings"
+        for column in tv1["column"]:
+            tv1.heading(column, text=column)
+
+        df_rows = detales.to_numpy().tolist()
+        for row in df_rows:
+            tv1.insert("", "end", values=row)
+            
+        return None
+    
+    # Semantine paieska
+    def Search():
+        clear_data()
+        
+        searching = str(search.get())
+        
+        
+        d1 = pd.DataFrame()
+        search_tokens = []
+        search_tokens.append(get_tokens(searching))
+        
+        relevant_word = relevant_words(search_tokens[0])
+        
+        k = 0
+        match_index = set()
+        for value in detales.Detalė.values:
+            for i in value.split():
+                if i.lower() in search_tokens[0]:
+                    match_index.add(k)
+                if i.lower() in relevant_word:
+                    match_index.add(k)
+            k += 1
+
+        k = 0
+        for value in detales.daugiau_info.values:
+            for i in value:
+                if i.lower() in search_tokens[0]:
+                    match_index.add(k)
+                if i.lower() in relevant_word:
+                    match_index.add(k)
+            k += 1
+            
+        k = 0
+        for value in detales.Info.values:
+            for i in value.split():
+                if i.lower() in search_tokens[0]:
+                    match_index.add(k)
+                if i.lower() in relevant_word:
+                    match_index.add(k)
+            k += 1
+            
+        for i in match_index:
+            d1 = d1.append(detales.iloc[i])
+        
+        cols = d1.columns.tolist()
+        cols = cols[:3] + [cols[4]] +[cols[3]]
+        d1 = d1[cols]
+        
+        tv1["column"] = list(d1.columns)
+        tv1["show"] = "headings"
+        for column in tv1["column"]:
+            tv1.heading(column, text=column)
+
+        df_rows = d1.to_numpy().tolist()
+        for row in df_rows:
+            tv1.insert("", "end", values=row)
+        return None
+    
+    # Istrinti senus duomenys
+    def clear_data():
+        tv1.delete(*tv1.get_children())
+
+
+    root.mainloop()
+    
+    
---
+++ GUI/scraping.py
@@ -0,0 +1,149 @@
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+from IPython.display import HTML
+
+# metodas gauti visas markes
+def get_markes():
+    page = requests.get("https://rrr.lt/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    mark_list = soup.find(class_ ="cars_list clearfix")
+
+    mark_list = mark_list.select(".cars_list__items a.cars_list__links")
+
+    mark_title ={ list(mark_list[sd].children)[0][:-1]:mark_list[sd]["href"] for sd in range(len(mark_list))}
+
+    markes = pd.DataFrame({
+        "Markes": mark_title.items(),
+    })
+    mark_list = []
+    for key, value in mark_title.items():
+        mark_list.append(key)
+    
+    return mark_title, mark_list # mark_title - biblioteka{marke: nuoroda}, mark_list - list(marke)
+
+
+# metodas, gauti visus modelius
+def get_modeliai(x, marks):
+    mark_title = marks
+    page = requests.get(mark_title[x])
+    soup = BeautifulSoup(page.content, 'html.parser')
+    models_list = soup.find(class_="models_hold")
+
+    models_list = models_list.select(".models .models__links")
+
+    model_title ={ models_list[sd]['data-label']:models_list[sd]["href"] for sd in range(len(models_list))}
+    
+    model_list = []
+    for key, value in model_title.items():
+        model_list.append(key)
+        
+    return model_title, model_list # model_title - biblioteka{modelis: nuoroda}, model_list - list(modelis)
+
+# Metodas, gauti kategorijas ir kieki
+def get_category(x, models):
+    model_title = models
+    page = requests.get(model_title[x])
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    parts = soup.find(class_ = "parts")
+    parts = parts.find_all(class_ = "parts__text")
+    
+    parts_links = soup.select(".parts_all-categories a")
+    parts_title ={list(parts[sd].children)[0][:-1].replace('\n                    ', '')+" >> "+str(list(parts[sd].children)[1].get_text()):parts_links[sd]["href"] for sd in range(len(parts))}
+
+    raktai = []
+    for key, value in parts_title.items():
+        if re.search("Šioje kategorijoje dalių neturime", key):
+            key = re.sub("Šioje kategorijoje dalių neturime", "(0)", key)
+        raktai.append(key)
+        
+ # Read Note below
+    
+    return parts_title, raktai # parts_title - biblioteka{kategorija: nuoroda}, raktai - list(kateogorijos)
+
+# gauti tos kategorijos prekes
+def get_detales(x, marke, parts):
+    parts_title = parts
+    parts_spec = []
+    parts_detales = []
+    
+    last = True
+    next_ = parts_title[x]
+    
+    while(last):
+        page = requests.get(next_)
+        soup = BeautifulSoup(page.content, 'html.parser')
+    
+        page_last = soup.find(class_ = "pages__items last") 
+        page_last = page_last.find(class_ = "pages__links")
+    
+        if page_last["class"] == ["pages__links"]:
+            next_ = page_last["href"]
+        else:
+            last = False
+        
+        parts_details =soup.select(".products__text p")
+        parts_specs = soup.select(".products .products__image")
+        parts_price = soup.select(".products .products__price")
+    
+        parts_spec += [parts_specs[sd]["title"] + " " + parts_specs[sd]["href"] + " " +  parts_price[sd].get_text() for sd in range(len(parts_specs))]
+    
+        parts_detales += [parts_details[sd].get_text() for sd in range(len(parts_details))]
+    
+    a = 0
+
+    item = []
+    links = []
+    kainos = []
+
+    url =  "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+    
+    # sukuriami list'ai, kuriuos idedu i  pandas df
+    for i in range(len(parts_spec)):
+        x = re.search(".+?(?=https:)", parts_spec[i])
+        y = re.search(url, parts_spec[i])
+        item.append(x.group())
+        links.append(y.group())
+        e = parts_spec[i].split()[-1]
+        k = parts_spec[i].split()[-2]
+        price = e + k
+        kainos.append(price)
+    
+    
+    
+    detales_info = [i for i in parts_detales if i.startswith(marke)]
+
+    detales = pd.DataFrame({
+        "Detalė": item,
+        "Kaina": kainos, 
+        "Info": detales_info,
+        "Nuoroda": links,
+    })
+    detales_info_list = []
+    
+    for inp in range(len(detales)):
+        pasirinkta_preke = detales.loc[inp, 'Nuoroda']
+
+        page = requests.get(pasirinkta_preke)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        detailed_info = soup.select(".product_desc .product_details")
+
+        detailed_test = [sd.get_text() for sd in soup.select(".product_desc .product_details")]
+        detailed_test = re.sub("\n", "|", detailed_test[1])
+        detailed_test = detailed_test.split("|")
+        
+        del detailed_test[detailed_test == '']
+        detales_info_dict = {}
+        detales_info_list2 = []
+        k = 0
+        
+        detales_info_list.append(detailed_test)
+   
+    detales["daugiau_info"] = detales_info_list
+    
+    return detales
