c60ef6ecf8b949a6582d24af968302b3bf571ed0
RolandGulbinovic <roland.gulbinovicvu@gmail.com>
2022-09-09 16:53:46 +0300

    Main message
    description messages aaa

--- test.py
+++ test.py
@@ -7,7 +7,5 @@ from IPython.display import HTML
 
 from scraping import get_markes, get_modeliai, get_category, get_detales
 from GUI import GUI
-global mark_list, mark_title
-aaaa
 mark_title, mark_lisst = get_markes()
 GUI(mark_list, mark_title)
